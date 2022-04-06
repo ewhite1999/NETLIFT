f0a110df881ad2af44f6204a495d31ae7a2c9046
ewhite1999 <ewhite1999@gmail.com>
2022-04-06 16:03:11 +0100

    add env variable to posts

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
-
 export const registerUser = async (e) => {
-  
   return async () => {
     try {
-      const { data } = await axios.post("http://127.0.0.1:5000/register", {
+      // eslint-disable-next-line no-undef
+      const { data } = await axios.post(`${process.env.API_URL}register`, {
         username: e.target.username.value,
         email: e.target.email.value,
-        password: e.target.password.value
+        password: e.target.password.value,
       });
       console.log(data);
       await loginUser(e);
@@ -22,23 +21,23 @@ export const loginUser = async (e) => {
   console.log("line 24");
   return async (dispatch) => {
     try {
-
       const username = e.target.username.value;
       console.log(username);
-      const { data } = await axios.post("http://127.0.0.1:5000/login", {
+      // eslint-disable-next-line no-undef
+      const { data } = await axios.post(`${process.env.API_URL}login`, {
         username: e.target.username.value,
-        password: e.target.password.value
+        password: e.target.password.value,
       });
       let token = `Bearer ${data.access_token}`;
       localStorage.setItem("token", token);
       dispatch({
         type: "LOGIN",
-        payload: { user: username, token: token }
+        payload: { user: username, token: token },
       });
     } catch (err) {
       dispatch({
         type: "SET_ERROR",
-        payload: err
+        payload: err,
       });
     }
   };
@@ -50,16 +49,16 @@ export const fetchProfile = () => {
       const token = localStorage.getItem("token");
 
       const { data } = await axios.get("http://127.0.0.1:5000/user", {
-        headers: { Authorization: token }
+        headers: { Authorization: token },
       });
       dispatch({
         type: "LOAD_USER",
-        payload: data
+        payload: data,
       });
     } catch (err) {
       dispatch({
         type: "SET_ERROR",
-        payload: err
+        payload: err,
       });
     }
   };
